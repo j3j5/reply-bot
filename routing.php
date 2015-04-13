5cd80224be42d919a87f9f9fa4947c9638529697
Julio Foulquié <jfoulquie@gmail.com>
2015-04-13 22:07:27 +0200

    Add basic-php-project.

---
+++ routing.php
@@ -0,0 +1,55 @@
+<?php
+
+// CLI execution
+if(PHP_SAPI == 'cli') {
+	$arguments_structure = array(
+		'controller',
+		'username',
+	);
+	if($argc >= 2) {
+		foreach($arguments_structure AS $key => $component) {
+			if(isset($argv[$key+1])) {
+				${$component} = $argv[$key+1];
+			}
+		}
+		if(!is_file(__DIR__ . DIRECTORY_SEPARATOR . "cli" . DIRECTORY_SEPARATOR . "$controller.php")) {
+			$controller = '404';
+		}
+	} else {
+		$controller = '404';
+		$username = '';
+	}
+	$controller = "cli" . DIRECTORY_SEPARATOR ."$controller.php";
+// WEBSERVER
+} else {
+	// The url is as follows http://yourserver.com/controller/username
+	$url_structure = array(
+		'controller',
+		'username'
+	);
+
+	$uri_segments = FALSE;
+	if(mb_strlen($_SERVER['REQUEST_URI']) > 1) { // Homepage has '/' as REQUEST_URI
+		$uri = $_SERVER['REQUEST_URI'];
+		// Remove GET parameters
+		if(mb_strpos($uri, '?') !== FALSE){
+			$uri = mb_substr($uri, 0, mb_strpos($uri, '?'));
+		}
+		$uri_segments = explode('/', $uri);
+	}
+	if(is_array($uri_segments)) {
+		foreach($url_structure AS $key => $component) {
+			if(isset($uri_segments[$key+1])) {
+				${$component} = $uri_segments[$key+1];
+			}
+		}
+		if(!is_file(__DIR__ . DIRECTORY_SEPARATOR . "controllers" . DIRECTORY_SEPARATOR . "$controller.php")) {
+			$controller = '404';
+		}
+	} else {
+		// Default controllers
+		$controller = 'landing';
+		$username = '';
+	}
+	$controller = "controllers" . DIRECTORY_SEPARATOR ."$controller.php";
+}
