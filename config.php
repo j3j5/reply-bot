bab0c2978c992a85f20b6079813dd7d526e595c7
Julio Foulquié <jfoulquie@gmail.com>
2015-04-16 00:50:01 +0200

    Add config file.

--- config.php
+++ config.php
@@ -1,17 +1,21 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
+use \Monolog\Logger;
+use \Monolog\Handler\StreamHandler;
+
+$local_config_file = __DIR__ . '/config.local.php';
 
 /**
  * Load here common styles and scripts for the page, you can add more on a per controller basis later
  */
 
-$css_files['header'][] = "//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css";
-$css_files['header'][] = "//netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css";
+// $css_files['header'][] = "//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css";
+// $css_files['header'][] = "//netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css";
 
-$js_files['footer'][] = "//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js";
-$js_files['footer'][] = "//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js";
-$js_files['footer'][] = "/js/global.js";
+// $js_files['footer'][] = "//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js";
+// $js_files['footer'][] = "//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js";
+// $js_files['footer'][] = "/js/global.js";
 
 /**
  * Config variables.
@@ -19,8 +23,22 @@ $js_files['footer'][] = "/js/global.js";
  */
 
 $twitter_settings = array(
-	'consumer_key'               => 'rZ0t5lQSRnjMQwP4gh4Tyw',
-	'consumer_secret'            => '15feSceFgs7abSZ15KvISno3TEL9AoowTwcXG2wJlw',
-	'token'                      => '229103126-C1tBp3mZDjzLKRbrzsi34d4iueaLvkvinbY9geda',
-	'secret'                     => 'tAJPbGDBfL3FzIulTV0XCfoYw6lIiewLRmbknYK3oU8MD',
+	'user_id'			=> 'THE_BOTs_ID',
+	'consumer_key'		=> 'YOUR_CONSUMER_KEY',
+	'consumer_secret'	=> 'YOUR_CONSUMER_SECRET',
+	'token'				=> 'YOUR_TOKEN',
+	'secret'			=> 'YOUR_SECRET',
+	'user_agent'		=> 'REPLY BOT'
 );
+
+$log = new Logger('reply-bot');
+if(PHP_SAPI == 'cli') {
+	$log->pushHandler(new StreamHandler("php://stdout", Logger::INFO));
+} else {
+	$log->pushHandler(new StreamHandler(dirname(__DIR__) . '/data/logs/reply-bot.log', Logger::DEBUG));
+}
+
+// Load local config
+if(is_file($local_config_file)) {
+	include($local_config_file);
+}
